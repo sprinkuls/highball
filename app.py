f26040ec98e72baf0a6e446d1a9298da8f9ea29b
sprinkuls <138340592+sprinkuls@users.noreply.github.com>
2026-06-19 21:59:50 -0400

    set up basic "status" message using htmx polling

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import threading
+
 import flask
 from flask import Flask, send_file, render_template
 
+import background
+
 app = Flask(__name__)
+
+shared_state: dict = {
+    "status_msg": "INITIAL STATUS MESSAGE",
+}
 
 
 @app.route("/index.html")
@@ -19,10 +27,32 @@ def favicon():
     return send_file("static/favicon.ico")
 
 
+# ====================
+# FUN
+# ====================
+
+@app.route("/status_msg")
+def status_msg():
+    return shared_state["status_msg"]
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("page_not_found.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1070)
+    t_flask = threading.Thread(target=lambda: app.run(host="0.0.0.0", port=1070, debug=False), daemon=True)
+    t_background = threading.Thread(target=lambda: background.run(shared_state), daemon=True)
+
+    t_flask.start()
+    t_background.start()
+
+    # uncomment this later
+    t_flask.join()
+    # while True:
+    #     print(f"Background Status: {shared_state["status_msg"]}")
+    #     sleep(1)
+
+    # you can also just do this if the web server is the last thing you need to run, i think:
+    # app.run(host="0.0.0.0", port=1070, debug=False)
---
+++ background.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+counter = 0
+
+
+def run(shared_state: dict):
+    global counter
+
+    while True:
+        sleep(3)
+        shared_state["status_msg"] = f"searching for '{counter}'"
+        counter += 1
