65d5222cabdac542cbf1acba2117907cded6212c
sprinkuls <138340592+sprinkuls@users.noreply.github.com>
2026-06-25 22:43:10 -0400

    use two `with` statements instead of contextmanager, improve class structure, add SearchTerm class
    i had assumed that wrapping the sqlite3.Connection would mean that both the sqlite3.Connection's __enter__ and __exit__ methods would be run in addition to my call to conn.close(), but that was not right! only conn.close() gets called, and nothing gets written to the database.

--- search_model.py
+++ search_model.py
@@ -1,58 +1,67 @@
 import sqlite3
-from contextlib import contextmanager
+from contextlib import closing
+
+
+class SearchTerm:
+    def __init__(self, id_: int, term: str):
+        self.id = id_
+        self.term = term
 
 
 class Search:
     database = 'searches.db'
 
-    with sqlite3.connect(database) as __conn:
-        __conn.execute("CREATE TABLE IF NOT EXISTS searches ("
-                       "id    INTEGER PRIMARY KEY,"
-                       "title TEXT    NOT NULL)")
-        __conn.execute("CREATE TABLE IF NOT EXISTS search_terms("
-                       "search_id INTEGER REFERENCES searches(id) ON DELETE CASCADE,"
-                       "term      TEXT    NOT NULL)")
-        __conn.commit()
-    __conn.close()
+    def __init__(self, id_: int, title: str, search_terms: list[SearchTerm]):
+        self.id = id_
+        self.title = title
+        self.search_terms = search_terms
 
-    @staticmethod
-    @contextmanager
-    def get_conn(db=database):
-        conn = sqlite3.connect(db)
-        try:
-            yield conn
-        finally:
-            conn.close()
+    @classmethod
+    def init_db(cls):
+        with closing(sqlite3.connect(cls.database)) as conn:
+            with conn:
+                conn.execute("CREATE TABLE IF NOT EXISTS searches ("
+                             "id    INTEGER PRIMARY KEY,"
+                             "title TEXT    NOT NULL)")
+                conn.execute("CREATE TABLE IF NOT EXISTS search_terms("
+                             "search_id INTEGER REFERENCES searches(id) ON DELETE CASCADE,"
+                             "term      TEXT    NOT NULL)")
+                conn.commit()
 
     # CREATE
     @classmethod
     def create_search(cls, title: str, terms: list[str]):
-        with cls.get_conn() as conn:
-            cur = conn.execute("INSERT INTO searches(title) VALUES (?)", (title,))
-            search_id = cur.lastrowid
-            for term in terms:
-                conn.execute("INSERT INTO search_terms(search_id, term) VALUES (?, ?)", (search_id, term))
+        with closing(sqlite3.connect(cls.database)) as conn:
+            with conn:
+                print("adding:", title, "with", terms)
+                cursor = conn.execute("INSERT INTO searches(title) VALUES (?)", (title,))
+                search_id = cursor.lastrowid
+                for term in terms:
+                    conn.execute("INSERT INTO search_terms(search_id, term) VALUES (?, ?)", (search_id, term))
 
     # READ
     @classmethod
     def get_all_searches(cls) -> list[tuple[str, str]]:
-        with cls.get_conn() as conn:
-            cur = conn.execute("SELECT * FROM searches")
-            return cur.fetchall()
+        with closing(sqlite3.connect(cls.database)) as conn:
+            with conn:
+                cursor = conn.execute("SELECT * FROM searches")
+                return cursor.fetchall()
 
     # READ
     @classmethod
     def get_search(cls, search_id: int) -> tuple[str, str] | None:
-        with cls.get_conn() as conn:
-            cur = conn.execute("SELECT * FROM searches WHERE id = (?)", (search_id,))
-            return cur.fetchone()
+        with closing(sqlite3.connect(cls.database)) as conn:
+            with conn:
+                cursor = conn.execute("SELECT * FROM searches WHERE id = (?)", (search_id,))
+                return cursor.fetchone()
 
     # DELETE
     @classmethod
     def delete_search(cls, search_id: int) -> bool | None:
-        with cls.get_conn() as conn:
-            cur = conn.execute("DELETE FROM searches WHERE id = (?)", (search_id,))
-            if cur.rowcount == 1:
-                return True
-            else:
-                return None
+        with closing(sqlite3.connect(cls.database)) as conn:
+            with conn:
+                cursor = conn.execute("DELETE FROM searches WHERE id = (?)", (search_id,))
+                if cursor.rowcount == 1:
+                    return True
+                else:
+                    return None
